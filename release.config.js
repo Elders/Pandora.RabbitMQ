a2d45d14e5c42101e3d15297923a53fb4cf30409
Kalina Shupeva <kalina.shupeva@eldersoss.com>
2025-01-27 16:08:36 +0200

    Adds ci

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports={
+  plugins: [
+    ["@semantic-release/commit-analyzer", {
+        releaseRules: [
+            {"type": "major"  , "release": "major"},
+            {"type": "release", "release": "major"},
+        ],
+        parserOpts: {
+            "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+    }],
+
+    ["@semantic-release/exec",{
+        prepareCmd: `
+            set -e
+            VER=\${nextRelease.version}
+            ##vso[build.updatebuildnumber]\${nextRelease.version}
+            dotnet pack "src/$PROJECT_DIR/"*.csproj -o "$STAGING_PATH" -p:Configuration=Release -p:PackageVersion=$VER --verbosity Detailed
+        `,
+        successCmd: `
+            set -e
+            echo "##vso[task.setvariable variable=newVer;]yes"
+        `,
+    }],
+    "@semantic-release/git"
+  ],
+
+  branches: [
+    'master',
+    {name: 'beta', channel: 'beta', prerelease: true},
+    {name: 'preview', channel: 'beta', prerelease: true}
+  ],
+}
